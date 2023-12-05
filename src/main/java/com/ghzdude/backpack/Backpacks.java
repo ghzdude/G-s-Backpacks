777f9e9279f2076b6713a3cb8f901ff1412c5c69
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-12-05 14:41:06 -0700

    initial setup add mui implementation

---
+++ src/main/java/com/ghzdude/backpack/Backpacks.java
@@ -0,0 +1,8 @@
+package com.ghzdude.backpack;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Backpacks.MODID)
+public class Backpacks {
+    public static final String MODID = "backpacks";
+}
