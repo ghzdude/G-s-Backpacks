5ef4e04a157e3df67e3ec5a5c23fcf63dbb1805c
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-12-05 21:43:16 -0700

    try implement inventory

--- src/main/java/com/ghzdude/backpack/items/BackpackItem.java
+++ src/main/java/com/ghzdude/backpack/items/BackpackItem.java
@@ -1,9 +1,15 @@
 package com.ghzdude.backpack.items;
 
 import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.manager.ClientGUI;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
+import com.cleanroommc.modularui.value.sync.ItemSlotSH;
+import com.cleanroommc.modularui.value.sync.SyncHandler;
+import com.cleanroommc.modularui.widgets.ItemSlot;
+import com.cleanroommc.modularui.widgets.SlotGroupWidget;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
 import com.ghzdude.backpack.BackpacksMod;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -14,10 +20,16 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BackpackItem extends Item {
 
+    IItemHandler handler;
+    List<SyncHandler> syncHandlers;
     public BackpackItem(String name) {
         ResourceLocation resourceLocation = new ResourceLocation(BackpacksMod.MODID, name);
         setRegistryName(resourceLocation);
@@ -25,6 +37,8 @@ public class BackpackItem extends Item {
         setTranslationKey(resourceLocation.getNamespace() + "." + resourceLocation.getPath());
         setMaxStackSize(1);
         BackpackItems.ITEMS.add(this);
+        handler = new ItemStackHandler(9);
+        syncHandlers = new ArrayList<>(handler.getSlots());
     }
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
@@ -36,10 +50,21 @@ public class BackpackItem extends Item {
     }
 
     public ModularScreen createGUI() {
-        ModularPanel panel = ModularPanel.defaultPanel("inventory");
-        panel.child(IKey.str("My first screen").asWidget()
-                .top(7).left(7));
+//        ModularPanel panel = ModularPanel.defaultPanel("inventory");
+        ModularPanel panel = new ModularPanel("inventory")
+                .size(100, 80)
+                .bindPlayerInventory();
+        SlotGroupWidget.Builder slotBuilder = SlotGroupWidget.builder()
+                .row("XXXXXXXXX")
+                .key('X', i -> {
+                    ItemSlot slot = new ItemSlot().slot(new ModularSlot(handler, i));
+                    syncHandlers.add(slot.getSyncHandler());
+                    return slot;
+                });
+
+        IWidget slotGroupWidget = slotBuilder.build().top(7 + 16).left(7);
+        IWidget title = IKey.str("Inventory").asWidget().top(7).left(7);
+        panel.child(slotGroupWidget).child(title);
         return new ModularScreen(BackpacksMod.MODID, panel);
     }
-
 }
