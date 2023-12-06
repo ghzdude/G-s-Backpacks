a77d227f738d8d6e2cfce7cdd975563763526965
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-12-05 18:18:49 -0700

    add initial item registration update mod annotation

--- src/main/java/com/ghzdude/backpack/Backpacks.java
+++ src/main/java/com/ghzdude/backpack/Backpacks.java
@@ -1,8 +1,20 @@
 package com.ghzdude.backpack;
 
+import com.ghzdude.backpack.items.BackpackItems;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = Backpacks.MODID)
+@Mod(modid = Backpacks.MODID,
+     name = "Ghzdude's Backpacks",
+     acceptedMinecraftVersions = "[1.12.2,1.13)",
+     dependencies = "required:forge; required-after:modularui@[2.3.1,)")
 public class Backpacks {
     public static final String MODID = "backpacks";
+
+    @SubscribeEvent
+    public void itemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(BackpackItems.backpackItem);
+    }
 }
---
+++ src/main/java/com/ghzdude/backpack/items/BackpackItems.java
@@ -0,0 +1,6 @@
+package com.ghzdude.backpack.items;
+
+public class BackpackItems {
+
+    public static BackpackItem backpackItem = new BackpackItem();
+}
