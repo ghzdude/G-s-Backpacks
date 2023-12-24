5d731d23f67e5f8d4a0a97675b41bd584218addc
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2023-12-23 19:38:11 -0700

    imports

--- src/main/java/com/ghzdude/backpack/BackpacksMod.java
+++ src/main/java/com/ghzdude/backpack/BackpacksMod.java
@@ -1,10 +1,6 @@
 package com.ghzdude.backpack;
 
-import com.ghzdude.backpack.items.BackpackItems;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = BackpacksMod.MODID,
      name = "Ghzdude's Backpacks",
