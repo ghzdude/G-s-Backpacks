30a34bcda5f7d5f09777fe408bccbfc7c8a946c0
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-01-02 15:00:56 -0700

    enable mixins add mixin for ModularContainer add bogosorter api

---
+++ src/main/java/com/ghzdude/backpack/mixin/GuiManagerMixin.java
@@ -0,0 +1,46 @@
+package com.ghzdude.backpack.mixin;
+
+import com.cleanroommc.modularui.screen.ModularContainer;
+import com.cleanroommc.modularui.widgets.slot.ModularSlot;
+import com.cleanroommc.modularui.widgets.slot.SlotGroup;
+import com.ghzdude.backpack.BackpacksMod;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.item.ItemStack;
+import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.MinecraftForge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import scala.tools.asm.Opcodes;
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+@Mixin(ModularContainer.class)
+public class GuiManagerMixin implements ILateMixinLoader {
+    @Override
+    public List<String> getMixinConfigs() {
+        return Arrays.asList("mixins.ghzbackpacks.json");
+    }
+
+    @Inject(method = "transferItem",
+            remap = false,
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            at = @At(target = "Ljava/lang/Math;min(II)I",
+                    shift = At.Shift.AFTER,
+                    by = 2,
+                    value = "INVOKE_ASSIGN"))
+    protected void transferItemMixin(ModularSlot fromSlot, ItemStack fromStack,
+                                     CallbackInfoReturnable<ItemStack> cir,
+                                     @Local(name = "maxSize") LocalRef<Integer> maxSize) {
+        maxSize.set(5000);
+        cir.cancel();
+    }
+}
