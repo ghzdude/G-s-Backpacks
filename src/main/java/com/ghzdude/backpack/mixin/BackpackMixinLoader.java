9e964a2cdf6babea3805dac78c6bb1806b53e647
Ghzdude <44148655+ghzdude@users.noreply.github.com>
2024-01-02 16:09:12 -0700

    try fix mixins

---
+++ src/main/java/com/ghzdude/backpack/mixin/BackpackMixinLoader.java
@@ -0,0 +1,13 @@
+package com.ghzdude.backpack.mixin;
+
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BackpackMixinLoader implements ILateMixinLoader {
+    @Override
+    public List<String> getMixinConfigs() {
+        return Arrays.asList("mixins.ghzbackpacks.json");
+    }
+}
--- src/main/java/com/ghzdude/backpack/mixin/modularui/ModularContainerMixin.java
+++ src/main/java/com/ghzdude/backpack/mixin/modularui/ModularContainerMixin.java
@@ -1,4 +1,4 @@
-package com.ghzdude.backpack.mixin;
+package com.ghzdude.backpack.mixin.modularui;
 
 import com.cleanroommc.modularui.screen.ModularContainer;
 import com.cleanroommc.modularui.widgets.slot.ModularSlot;
@@ -8,31 +8,19 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import zone.rong.mixinbooter.ILateMixinLoader;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Mixin(ModularContainer.class)
-public class GuiManagerMixin implements ILateMixinLoader {
-    @Override
-    public List<String> getMixinConfigs() {
-        return Arrays.asList("mixins.ghzbackpacks.json");
-    }
+public abstract class ModularContainerMixin {
 
     @Inject(method = "transferItem",
             remap = false,
-            locals = LocalCapture.CAPTURE_FAILHARD,
+            locals = LocalCapture.PRINT,
             at = @At(target = "Ljava/lang/Math;min(II)I",
                     shift = At.Shift.AFTER,
-                    by = 2,
                     value = "INVOKE_ASSIGN"))
     protected void transferItemMixin(ModularSlot fromSlot, ItemStack fromStack,
-                                     CallbackInfoReturnable<ItemStack> cir,
-                                     @Local(name = "maxSize") LocalRef<Integer> maxSize) {
+                                                   @Local(ordinal = 9) LocalRef<Integer> maxSize) {
         maxSize.set(5000);
-        cir.cancel();
     }
 }
